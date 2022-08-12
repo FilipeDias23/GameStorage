912fc8d7343f658df789cd0e6086360def41977a
filipe dias <filipediasit@gmail.com>
2022-08-12 18:37:42 +0100

    added the controllers methods

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,6 @@
 class GamesController < ApplicationController
   def index
-
+    @games = Games.all
   end
 
   def show
@@ -8,11 +8,12 @@ class GamesController < ApplicationController
   end
 
   def new
-
+    @game = Games.new
   end
 
   def create
-
+    @game = Games.new(games_params)
+    @game.save
   end
 
   def edit
@@ -25,5 +26,11 @@ class GamesController < ApplicationController
 
   def destroy
 
+  end
+
+  private
+
+  def games_params
+    params.require(:game).permit(:name, :genre, :price, :platform, :condition)
   end
 end
