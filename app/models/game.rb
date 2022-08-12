c397948d085d55364dd94e660aaf89f6ac891f1d
filipe dias <filipediasit@gmail.com>
2022-08-12 15:50:02 +0100

    models validations and routes defined

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,7 @@
 class Game < ApplicationRecord
+  belongs_to :users
+  has_one :booking, dependent: :destroy
+  validates :name, :genre, :platform, :condition, presence: true, absence: true
+  validates :name, length: { in: 6..30 }, absence: true
+  validates :price, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_many :games, dependent: :destroy
+  has_many :bookings, dependent: :destroy
+  validates :first_name, :last_name, :username, presence: true, length: { in: 2..20 }, absence: true
+  validates :age, presence: true, { only_integer: true }
+  validates :age, comparison: { greater_than: 16 }
+  validades :email, presence: true, absence: true
+  validates :location, presence: true, absence: true
 end
