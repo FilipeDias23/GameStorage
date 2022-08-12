b80341e975a5b9abcca5f0ffcb8794015d8145ca
filipe dias <filipediasit@gmail.com>
2022-08-12 18:45:48 +0100

    add a migration to add an img_url to the games model

---
+++ db/migrate/20220812174026_add_image_to_games.rb
@@ -0,0 +1,5 @@
+class AddImageToGames < ActiveRecord::Migration[6.1]
+  def change
+    add_column :games, :image_url, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_12_133344) do
+ActiveRecord::Schema.define(version: 2022_08_12_174026) do
 
   create_table "bookings", force: :cascade do |t|
     t.integer "user_id"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2022_08_12_133344) do
     t.string "condition"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_url"
   end
 
   create_table "users", force: :cascade do |t|
