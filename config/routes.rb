8733bdd1b317ad3ffe2a0eb6b293e428df3be2cf
filipe dias <filipediasit@gmail.com>
2022-08-19 16:11:01 +0100

    changed the games routes to default json

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :games do
+  resources :games, defaults: { format: 'json'} do
     resources :bookings, only: [ :index, :show, :new, :create ]
   end
 end
